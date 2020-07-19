5c16e75e68c1b54386f8a339a6a642d8c8cc70a3
kbufhv <kbu2381@students.fhv.at>
2020-07-19 09:19:51 +0200

    first commit

---
+++ src/Particle_Temp_Hum_To_Azure_IoTHub.cpp
@@ -0,0 +1,42 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/kathrinburtscher/Particle/Particle_Temp_Hum_To_Azure_IoTHub/src/Particle_Temp_Hum_To_Azure_IoTHub.ino"
+/*
+ * Project Particle_Temp_Hum_To_Azure_IoTHub
+ */
+
+#include "Grove_Temperature_And_Humidity_Sensor.h"
+
+//Temperature and Humidity Sensor
+void loop();
+#line 8 "/Users/kathrinburtscher/Particle/Particle_Temp_Hum_To_Azure_IoTHub/src/Particle_Temp_Hum_To_Azure_IoTHub.ino"
+DHT sensor(D2);
+String temp;
+String hum;
+
+// setup() runs once, when the device is first turned on.
+void setup()
+
+  //Temperature and Humidity Sensor
+  Serial.begin(9600);
+  sensor.begin();
+}
+
+// loop() runs over and over again (delay duration can be manipulated in the Particle App)
+void loop()
+{
+
+  //get sensor data
+  temp = String(sensor.getTempCelcius());
+  hum = String(sensor.getHumidity());
+
+  //AZURE build data string and publish data
+  String res = "{\"temperature\":" + temp + ", \"humidity\":" + hum + "}";
+  Particle.publish("temperature", res, PRIVATE);
+
+  //wait for 30 seconds
+  delay(30 * 1000);
+}
